c50fd3bb1cd91385770cb12340239d4db64c63b4
ThePhilosopherV <42734412+ThePhilosopherV@users.noreply.github.com>
2022-01-07 22:16:28 +0100

    Add files via upload

--- source-code/asm.asm
+++ source-code/asm.asm
@@ -55,11 +55,148 @@ call load_f
 mov bx,0x8000
 mov es,bx
 mov bx,0
-;;;;;;;;;;;; lets process  of dear source file
+
+
+
+mov dx,1    ;line counter
+;;;;;;;;;;;; lets process  our dear source file
+skip_spacesonfirstline:
+
+cmp byte [es:bx],space
+
+jne checkinst
+inc bx
+
+jmp skip_spacesonfirstline
+
+checkinst:
+
+mov si,mov_in
+
+mov cx,0
+;;;;;;;;;;;;;;is it  mov in ? mov instruction processing
+movinstl:
+
+lodsb
+
+cmp al,byte [es:bx]
+
+jne checkvald
+
+inc bx
+inc cx
+jmp movinstl
+
+checkvald:
+
+cmp cx,3
+je pmov
+
+jmp asm_ret
+
+;;;;;;;;;;;; here we're sure it is
+pmov:
+
+;lets  skip spaces
+
+mov cx,0
+skip_spacess:
+
+cmp byte [es:bx],space
+
+jne check_inst
+inc bx
+inc cx
+jmp skip_spacess
+
+
+
+check_inst:
+cmp cx,0
+
+je error
+
+
+
+;;;;;;;;;;;;;;check register
+
+
+cmp  word [es:bx],"al"
+je p_al
+
+
+jmp asm_ret
+
+
+
+p_al:
+add bx,2
+
+
+
+skip_spaces_al:
+
+cmp byte [es:bx],space
+
+jne check_in_al
+inc bx
+
+jmp skip_spaces_al
+
+check_in_al:
+cmp byte [es:bx],","
+jne error
 
 
 
 
+inc bx
+skip_spaces_al_aftecomma:
+
+cmp byte [es:bx],space
+
+jne check_in_al_arg
+inc bx
+
+jmp skip_spaces_al_aftecomma
+
+check_in_al_arg:
+
+
+mov al,byte [es:bx]
+;add al,48
+call pchar
+
+call pause
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+error:
+mov si,er_msg
+call newLine
+
+
+call printn
+mov al,dl
+add al,48
+call pchar
+
+jmp asm_ret
 
 noarg_error:
 mov si,noargs_error_str
@@ -79,3 +216,5 @@ asm_command:    db   "asm"
 space:  equ      " "
 ;filename:   db  50 dup   0
 noargs_error_str:   db    "Missing file name argument",0
+mov_in:   db    "mov"
+er_msg:    db    "Error in line ",0
